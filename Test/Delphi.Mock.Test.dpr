9ebdcbe503dda27a355238440429dd7169636633
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2021-02-08 10:01:55 -0300

    Leak report in file.

--- Test/Delphi.Mock.Test.dpr
+++ Test/Delphi.Mock.Test.dpr
@@ -34,6 +34,12 @@ var
   nunitLogger : ITestLogger;
 {$ENDIF}
 begin
+  FastMM_OutputDebugStringEvents := [];
+  FastMM_LogToFileEvents := [mmetUnexpectedMemoryLeakSummary];
+  FastMM_MessageBoxEvents := [mmetDebugBlockDoubleFree, mmetDebugBlockReallocOfFreedBlock, mmetVirtualMethodCallOnFreedObject];
+
+  FastMM_DeleteEventLogFile;
+
 {$IFDEF TESTINSIGHT}
   TestInsight.DUnitX.RunRegisteredTests;
 {$ELSE}
